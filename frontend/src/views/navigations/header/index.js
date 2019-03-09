9304b0c7779b0cca5144c20045c8c46f2436bc15
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-03-09 10:49:37 +0530

    Add google login button!

--- frontend/src/views/navigations/header/index.js
+++ frontend/src/views/navigations/header/index.js
@@ -6,11 +6,16 @@ const NavigationHeader = () =>
     <div className="top-bar-left">
       <ul className="dropdown menu">
         <li className="menu-text">Live Stream App</li>
+        <li><Link to="/">All Streams</Link></li>
       </ul>
     </div>
     <div className="top-bar-right">
       <ul className="menu">
-        <li><Link to="/">All Streams</Link></li>
+        <li>
+          <button type="button" className="button radius bordered shadow social google">
+            Login with Google
+          </button>
+        </li>
       </ul>
     </div>
   </div>;
