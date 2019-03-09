517ae7c0ebe6b3cd8a6bdbf67f19e2e542248842
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-03-09 15:10:59 +0530

    Signed in state logic!

--- frontend/src/views/components/google-auth/index.js
+++ frontend/src/views/components/google-auth/index.js
@@ -3,18 +3,35 @@ import React, { Component } from 'react';
 import googleCredentials from '../../../config/google';
 
 const GoogleAuth = class extends Component {
+  state = {
+    hasSignedInState: null,
+  };
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       const { clientId } = googleCredentials;
-      console.log({ clientId });
-      window.gapi.client.init({ clientId });
+      const scope = 'email';
+      window.gapi.client
+        .init({ clientId, scope })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          const { isSignedIn } = this.auth;
+          this.handleAuthChange();
+          isSignedIn.listen(this.handleAuthChange);
+        });
     });
   }
 
+  handleAuthChange = () => {
+    const hasSignedInState = this.auth.isSignedIn.get();
+    this.setState({ hasSignedInState });
+  };
+
   render() {
+    const { hasSignedInState } = this.state;
     return (
       <button type="button" className="button radius bordered shadow social google">
-        Login with Google
+        { (!hasSignedInState) ? <div>Login with Google</div> : <div>My Account</div> }
       </button>
     );
   }
