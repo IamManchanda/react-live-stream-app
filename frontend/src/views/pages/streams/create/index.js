6b62ae5a3fda611234378b5433bd73ffc319f7f1
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-03-16 17:36:51 +0530

    Refactor the callback functions into Named ones!

--- frontend/src/views/pages/streams/create/index.js
+++ frontend/src/views/pages/streams/create/index.js
@@ -5,12 +5,12 @@ import streamCreationData from './streamCreationData';
 
 const validateForm = (formValues) => {
   const errors = {};
-  const getErrorContent = (itemName) =>
-    streamCreationData.filter((item) => item.name === itemName)[0].errorContent;
-  if (!formValues.title) 
-    errors.title = getErrorContent('title');
-  if (!formValues.description) 
-    errors.description = getErrorContent('description');
+  const getErrorContent = (itemName) => 
+    streamCreationData.filter(function filterStreamCreationData(item) {
+      return item.name === itemName;
+    })[0].errorContent;
+  if (!formValues.title) errors.title = getErrorContent('title');
+  if (!formValues.description) errors.description = getErrorContent('description');
   return errors;
 };
 
@@ -23,9 +23,13 @@ const StreamCreate = class extends Component {
     const { handleSubmit } = this.props;
     return (
       <form onSubmit={ handleSubmit(this.onStreamCreation) }>
-        { streamCreationData.map(({ id, name, component: DynFieldComponent, ownProps }) => 
-            <Field key={ id } name={ name } 
-              component={ DynFieldComponent } { ...ownProps } /> ) }
+        { streamCreationData.map(function iterateStreamCreationData(item) {
+            const { id, name, component: DynFieldComponent, ownProps = {} } = item;
+            return <Field 
+              key={ id } name={ name } 
+              component={ DynFieldComponent } { ...ownProps } 
+            />;
+          }) }
         <input type="submit" className="button" value="Submit" />
       </form>
     );
--- frontend/src/views/routes/index.js
+++ frontend/src/views/routes/index.js
@@ -6,10 +6,13 @@ import routesData from './routesData';
 const Routes = () => 
   <div className="grid-x grid-padding-x grid-padding-y">
     <div className="cell">
-      { routesData.map(({ id, path, page: DynRoutedPage, ownProps = {} }) =>
-          <Route exact key={ id } path={ path } 
-            render={ (props = {}) => 
-              <DynRoutedPage { ...props } { ...ownProps } /> } /> ) }
+      { routesData.map(function iterateRoutesData(item) {
+          const { id, path, page: DynRoutedPage, ownProps = {} } = item;
+          return <Route 
+            exact key={ id } path={ path } 
+            render={ (props = {}) => <DynRoutedPage { ...props } { ...ownProps } /> } 
+          />;
+        }) }
     </div>
   </div>;
 
